7cc76e20d330c2df4302207a17f26ec1bc418911
hp-77 <harsh.pant.77@gmail.com>
2024-07-11 16:45:41 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
+// const nextConfig = {
+//   reactStrictMode: false,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
+  swcMinify: false, // Disable SWC minification
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Disable Terser minification
+    config.optimization.minimize = false;
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
