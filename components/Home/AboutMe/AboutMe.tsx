7067e39db00c0f94d7fc7b875d559b7dba808856
hp-77 <149194659+hp-77@users.noreply.github.com>
2024-07-11 15:04:31 +0530

    Update AboutMe.tsx

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -1,49 +1,45 @@
 import React from "react";
 import Img from "../../../components/smallComp/image/Img";
 import ArrowIcon from "../../../components/Icons/ArrowIcon";
+
 export default function AboutMe(props) {
   const technologies = [
     ["Next.js", "JavaScript (ES6+)", "Tailwind CSS", "MongoDB", "Expressjs"],
     ["Node.js", "Firebase", "Stream.io", "AWS", "ReactJS"],
   ];
   return (
-    <div id="aboutSection" data-aos="fade-up" className="snap-start flex flex-col  items-center py-20 bg-AAprimary">
-
+    <div id="aboutSection" data-aos="fade-up" className="snap-start flex flex-col items-center py-20 bg-AAprimary">
       <div
         className="flex flex-col space-y-8 px-4 sm:px-0 w-full sm:w-[500px] 
         md:w-[700px] lg:w-[900px] "
       >
         <div className="flex flex-row items-center ">
-          <div className="flex flex-row  items-center mr-4">
+          <div className="flex flex-row items-center mr-4">
             <ArrowIcon className={"flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"} />
-            <span className="text-AAsecondary font-Header text-sm  sm:text-xl"> 01.</span>
+            <span className="text-AAsecondary font-Header text-sm sm:text-xl"> 01.</span>
             <span className="flex-none text-gray-200 opacity-85 font-bold tracking-wider text-lg sm:text-2xl pl-4">
               About Me
             </span>
           </div>
           <div className="bg-gray-400 h-[0.2px] w-full sm:w-72 ml-4"></div>
         </div>
-        
 
-        <div className="w-full flex flex-col md:flex-row space-y-8 md:space-y-0  md:space-x-8 sm:space-x-2 ">
+        <div className="w-full flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 sm:space-x-2 ">
           <div className="w-full md:w-7/12 space-y-4 sm:text-base text-sm ">
             <div className="font-Header ">
               <span className="text-gray-400 ">
-              I'm <span className="text-AAsecondary">Harsh Pant</span>, a passionate and detail-oriented software development enthusiast with a proven track record in both academics and professional experience. I earned a stellar <span className="text-AAsecondary">97% in the CBSE Secondary Examination</span> , showcasing my commitment to academic excellence. In the highly competitive <span className="text-AAsecondary">JEE MAIN , I achieved an outstanding 99.3 percentile</span> , underscoring my strong aptitude for technical problem-solving and analytical thinking
+                I&apos;m <span className="text-AAsecondary">Harsh Pant</span>, a passionate and detail-oriented software development enthusiast with a proven track record in both academics and professional experience. I earned a stellar <span className="text-AAsecondary">97% in the CBSE Secondary Examination</span>, showcasing my commitment to academic excellence. In the highly competitive <span className="text-AAsecondary">JEE MAIN, I achieved an outstanding 99.3 percentile</span>, underscoring my strong aptitude for technical problem-solving and analytical thinking.
               </span>
             </div>
             <div className="font-Header ">
               <span className="text-gray-400 ">
-              Currently, I am honing my skills as a remote{" "}
-                <span className="text-AAsecondary">intern in Integra works, Dubai </span> where I contribute as a full-stack developer. My focus lies in leveraging cutting-edge technologies such as Next.js and Docker to build scalable and efficient web applications. My experience extends to competitive programming on CodeChef, where I hold a <span className="text-AAsecondary">distinguished 5-star rating</span>, demonstrating my proficiency in algorithmic design and computational thinking.{" "}
-                
+                Currently, I am honing my skills as a remote <span className="text-AAsecondary">intern in Integra works, Dubai</span> where I contribute as a full-stack developer. My focus lies in leveraging cutting-edge technologies such as Next.js and Docker to build scalable and efficient web applications. My experience extends to competitive programming on CodeChef, where I hold a <span className="text-AAsecondary">distinguished 5-star rating</span>, demonstrating my proficiency in algorithmic design and computational thinking.
               </span>
             </div>
-            
 
             <div className="font-Header tracking-wide">
-              <span className="text-gray-400  ">
-                Here are a few technologies I&apos;ve been working with recently :
+              <span className="text-gray-400 ">
+                Here are a few technologies I&apos;ve been working with recently:
               </span>
             </div>
             <div className="font-Header tracking-wide flex flex-row space-x-16">
@@ -81,7 +77,7 @@ export default function AboutMe(props) {
                translate-y-5 rounded"
             ></div>
 
-            <div className="absolute w-5/6 h-5/6  rounded overflow-hidden">
+            <div className="absolute w-5/6 h-5/6 rounded overflow-hidden">
               <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-10 duration-300 rounded overflow-hidden"></div>
               <Img
                 src={"/img/Portfolio-portrait-3.jpeg"}
@@ -91,8 +87,8 @@ export default function AboutMe(props) {
             </div>
           </div>
           {/* // ?Image in Mobile */}
-          <div className=" relative w-full h-48 md:hidden  flex justify-center items-center">
-            <div className="absolute w-48 h-full  rounded  translate-x-5 translate-y-5 border-2 border-AAsecondary"></div>
+          <div className="relative w-full h-48 md:hidden flex justify-center items-center">
+            <div className="absolute w-48 h-full rounded translate-x-5 translate-y-5 border-2 border-AAsecondary"></div>
             <div className="absolute w-48 h-full rounded overflow-hidden">
               <Img
                 src={"/img/Portfolio-portrait-3.jpeg"}
@@ -100,10 +96,123 @@ export default function AboutMe(props) {
                 alt="My Image Not Found"
               />
             </div>
-            <div className="absolute w-48 h-full  bg-AAsecondary opacity-10 md:opacity-60  rounded overflow-hidden"></div>
+            <div className="absolute w-48 h-full bg-AAsecondary opacity-10 md:opacity-60 rounded overflow-hidden"></div>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+
+
+
+// import React from "react";
+// import Img from "../../../components/smallComp/image/Img";
+// import ArrowIcon from "../../../components/Icons/ArrowIcon";
+// export default function AboutMe(props) {
+//   const technologies = [
+//     ["Next.js", "JavaScript (ES6+)", "Tailwind CSS", "MongoDB", "Expressjs"],
+//     ["Node.js", "Firebase", "Stream.io", "AWS", "ReactJS"],
+//   ];
+//   return (
+//     <div id="aboutSection" data-aos="fade-up" className="snap-start flex flex-col  items-center py-20 bg-AAprimary">
+
+//       <div
+//         className="flex flex-col space-y-8 px-4 sm:px-0 w-full sm:w-[500px] 
+//         md:w-[700px] lg:w-[900px] "
+//       >
+//         <div className="flex flex-row items-center ">
+//           <div className="flex flex-row  items-center mr-4">
+//             <ArrowIcon className={"flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"} />
+//             <span className="text-AAsecondary font-Header text-sm  sm:text-xl"> 01.</span>
+//             <span className="flex-none text-gray-200 opacity-85 font-bold tracking-wider text-lg sm:text-2xl pl-4">
+//               About Me
+//             </span>
+//           </div>
+//           <div className="bg-gray-400 h-[0.2px] w-full sm:w-72 ml-4"></div>
+//         </div>
+        
+
+//         <div className="w-full flex flex-col md:flex-row space-y-8 md:space-y-0  md:space-x-8 sm:space-x-2 ">
+//           <div className="w-full md:w-7/12 space-y-4 sm:text-base text-sm ">
+//             <div className="font-Header ">
+//               <span className="text-gray-400 ">
+//               I'm <span className="text-AAsecondary">Harsh Pant</span>, a passionate and detail-oriented software development enthusiast with a proven track record in both academics and professional experience. I earned a stellar <span className="text-AAsecondary">97% in the CBSE Secondary Examination</span> , showcasing my commitment to academic excellence. In the highly competitive <span className="text-AAsecondary">JEE MAIN , I achieved an outstanding 99.3 percentile</span> , underscoring my strong aptitude for technical problem-solving and analytical thinking
+//               </span>
+//             </div>
+//             <div className="font-Header ">
+//               <span className="text-gray-400 ">
+//               Currently, I am honing my skills as a remote{" "}
+//                 <span className="text-AAsecondary">intern in Integra works, Dubai </span> where I contribute as a full-stack developer. My focus lies in leveraging cutting-edge technologies such as Next.js and Docker to build scalable and efficient web applications. My experience extends to competitive programming on CodeChef, where I hold a <span className="text-AAsecondary">distinguished 5-star rating</span>, demonstrating my proficiency in algorithmic design and computational thinking.{" "}
+                
+//               </span>
+//             </div>
+            
+
+//             <div className="font-Header tracking-wide">
+//               <span className="text-gray-400  ">
+//                 Here are a few technologies I&apos;ve been working with recently :
+//               </span>
+//             </div>
+//             <div className="font-Header tracking-wide flex flex-row space-x-16">
+//               <div className="flex flex-row space-x-2 items-center">
+//                 <div className="flex flex-col space-y-4 sm:text-base text-sm">
+//                   {technologies[0].map((tech, index) => {
+//                     return (
+//                       <div key={index} className="flex flex-row items-center space-x-2">
+//                         <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
+//                         <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+//                       </div>
+//                     );
+//                   })}
+//                 </div>
+//               </div>
+//               <div className="flex flex-row space-x-2 items-center">
+//                 <div className="flex flex-col space-y-4 sm:text-base text-sm">
+//                   {technologies[1].map((tech, index) => {
+//                     return (
+//                       <div key={index} className="flex flex-row items-center space-x-2">
+//                         <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
+//                         <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+//                       </div>
+//                     );
+//                   })}
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           {/* // ? Image in Desktop and Tablet */}
+//           <div className="group relative lg:w-96 lg:h-96 md:w-72 md:h-72 md:block hidden ">
+//             <div
+//               className="group-hover:translate-x-3 group-hover:translate-y-3
+//                duration-300 absolute w-5/6 h-5/6 border-2 border-AAsecondary translate-x-5 
+//                translate-y-5 rounded"
+//             ></div>
+
+//             <div className="absolute w-5/6 h-5/6  rounded overflow-hidden">
+//               <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-10 duration-300 rounded overflow-hidden"></div>
+//               <Img
+//                 src={"/img/Portfolio-portrait-3.jpeg"}
+//                 className={"object-contain rounded-lg"}
+//                 alt="My Image Not Found"
+//               />
+//             </div>
+//           </div>
+//           {/* // ?Image in Mobile */}
+//           <div className=" relative w-full h-48 md:hidden  flex justify-center items-center">
+//             <div className="absolute w-48 h-full  rounded  translate-x-5 translate-y-5 border-2 border-AAsecondary"></div>
+//             <div className="absolute w-48 h-full rounded overflow-hidden">
+//               <Img
+//                 src={"/img/Portfolio-portrait-3.jpeg"}
+//                 className={"object-contain rounded-lg"}
+//                 alt="My Image Not Found"
+//               />
+//             </div>
+//             <div className="absolute w-48 h-full  bg-AAsecondary opacity-10 md:opacity-60  rounded overflow-hidden"></div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
